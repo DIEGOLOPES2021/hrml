d2d1499169d8c1332bc6d3e7c8ec1a2bbd388aa5
Diego <dl604291@gmail.com>
2021-02-17 00:15:20 -0300

    17/02/2021

---
+++ Atividade/academia/src/br/com/academia/model/NivelDeAtividade.java
@@ -0,0 +1,27 @@
+package br.com.academia.model;
+
+import br.com.academia.model.Pessoa;
+
+public class NivelDeAtividade {
+	
+	
+	public static  NivelDeAtividade LEVE = null;
+	public static  NivelDeAtividade MODERADO = null;
+	public static  NivelDeAtividade INTENSO = null;
+
+//	public double getNcd() {
+//		double ncd =0;
+//		if (this.getSexo == 'M') {
+//		
+//		}
+//		return ncd;
+//	}
+	
+	
+	
+}
+
+
+
+
+
